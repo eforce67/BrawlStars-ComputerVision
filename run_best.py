03285a51d75009c1ae0a90104d705dd0071d1acc
Eforce <47955696+eforce67@users.noreply.github.com>
2024-04-27 20:55:57 -0800

    Update run_best.py
    updated to support manual aim and reverse some variables position

--- run_best.py
+++ run_best.py
@@ -9,8 +9,7 @@ from train import clean_inputs, load_image_and_detect
 
 
 def load_model(model_path, config_path):
-    """
-    Load the trained model and configuration.
+    """ Load the trained model and configuration.
 
     Args:
         model_path (str): Path to the saved model file.
@@ -21,7 +20,6 @@ def load_model(model_path, config_path):
     """
     with open(model_path, 'rb') as f:
         model = pickle.load(f)
-
     config = neat.Config(
         neat.DefaultGenome,
         neat.DefaultReproduction,
@@ -29,25 +27,23 @@ def load_model(model_path, config_path):
         neat.DefaultStagnation,
         config_path
     )
-
     return model, config
 
 def run_model(genome, config, emulator_name):
-    """
-    Run the trained model on the game.
+    """ Run the trained model on the game.
 
     Args:
         genome: The trained genome.
         config: The configuration for the NEAT algorithm.
     """
-    start_time = time()
-    time_limit = 207
-    thread = None
-
     # Create the neural network from the genome and configuration
     network = neat.ctrnn.CTRNN.create(genome, config, 0.01)
-    control_instance = controller.LDPlayerInstance(emulator_name)
+    control_instance = controller.ControllerInstance(emulator_name)
     control_instance.press_game()
+    
+    time_limit = 207
+    thread = None
+    start_time = time()
 
     while True:
         control_instance.screen_shot(emulator_name)
@@ -55,6 +51,7 @@ def run_model(genome, config, emulator_name):
         inputs = clean_inputs(prediction)
         if (inputs.victory | inputs.defeat | inputs.draw) == 1:
             break
+
         current_time = time()
         elapsed_time = current_time - start_time
 
@@ -63,18 +60,19 @@ def run_model(genome, config, emulator_name):
         action_taken = np.argmax(output)
 
         # Execute the action based on the output
-        if action_taken in range(5):
+        if action_taken in range(3):
             if thread is not None:
                 thread.stop()
                 thread = None
-            thread = controller.start_action(action_taken, control_instance)
+            thread = controller.start_action(action_taken, control_instance, inputs.visible_enemy, inputs.visible_enemy_epoint)
         else:
-            controller.start_action(action_taken, control_instance)
+            controller.start_action(action_taken, control_instance, inputs.visible_enemy, inputs.visible_enemy_epoint)
 
         # Exit the game if the time limit is reached
         if elapsed_time >= time_limit:
             print("3 minutes & 45 seconds have elapsed.")
             break
+
     print('finished playing the game')
     if thread is not None: thread.stop()
     
