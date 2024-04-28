7284e48702dc46a5413b16ff463fd1d68e6ba9d5
Eforce <47955696+eforce67@users.noreply.github.com>
2024-04-27 21:02:54 -0800

    Update controller.py
    added in manual aim logic and fixed some issues with non-corresponding buttons & numbers

--- controller.py
+++ controller.py
@@ -1,20 +1,57 @@
+import ctypes
 import threading
 import time
 
-import win32api
 import win32con
 import win32gui
 import win32ui
 from PIL import Image
 from pynput.keyboard import Key
+from load_config import (PRESS_AND_HOLD, RANGE_LIMIT, SHOT_DELAY,
+                         TAP_AND_RELEASE)
 
-import load_config
+# Define constants
+MOUSEEVENTF_RIGHTDOWN = 0x0008
+MOUSEEVENTF_RIGHTUP = 0x0010
 
+# Define SendInput structures
+PUL = ctypes.POINTER(ctypes.c_ulong)
+
+class KeyBdInput(ctypes.Structure):
+    _fields_ = [("wVk", ctypes.c_ushort),
+                ("wScan", ctypes.c_ushort),
+                ("dwFlags", ctypes.c_ulong),
+                ("time", ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+class HardwareInput(ctypes.Structure):
+    _fields_ = [("uMsg", ctypes.c_ulong),
+                ("wParamL", ctypes.c_short),
+                ("wParamH", ctypes.c_ushort)]
+
+class MouseInput(ctypes.Structure):
+    _fields_ = [("dx", ctypes.c_long),
+                ("dy", ctypes.c_long),
+                ("mouseData", ctypes.c_ulong),
+                ("dwFlags", ctypes.c_ulong),
+                ("time", ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+class Input_I(ctypes.Union):
+    _fields_ = [("ki", KeyBdInput),
+                ("mi", MouseInput),
+                ("hi", HardwareInput)]
+
+class Input(ctypes.Structure):
+    _fields_ = [("type", ctypes.c_ulong),
+                ("ii", Input_I)]
 
 class ActionThread(threading.Thread):
-    def __init__(self, action, instance):
+    def __init__(self, action, instance, enemy_distance, eppoint):
         super(ActionThread, self).__init__()
         self.action = action
+        self.enemy_position = eppoint
+        self.enemy_distance = enemy_distance
         self._keep_running = True
         self.ACTIONS = {
             0: instance.move_up,
@@ -22,32 +59,35 @@ class ActionThread(threading.Thread):
             2: instance.move_left,
             3: instance.move_right,
             4: instance.auto_aim,
-            5: instance.activate_gadget,
-            6: instance.activate_super,
-            7: instance.activate_hypercharge,
+            5: instance.manual_aim,
+            6: instance.activate_gadget,
+            7: instance.activate_super,
+            8: instance.activate_hypercharge,
         }
 
     def stop(self):
         self._keep_running = False
 
     def run(self):
-        while self._keep_running and self.action in range(0,4):
+        while self._keep_running and self.action in range(3):
             self.execute_action()
         else:
             self.execute_action()
 
     def execute_action(self):
         action_func = self.ACTIONS.get(self.action, None)
-        if action_func:
+        if action_func and (self.action != 5):
             action_func()
+        elif action_func and self.action == 5 and self.enemy_position != None:
+            action_func(self.enemy_distance, self.enemy_position)
             
-class LDPlayerInstance:
+class ControllerInstance:
     def __init__(self, window_name):
         self.window_name = window_name
-        hwndMain = win32gui.FindWindow(None, window_name)
-        self.hwndChild = win32gui.GetWindow(hwndMain, win32con.GW_CHILD)
+        self.hwndMain = win32gui.FindWindow(None, window_name)
+        self.hwndChild = win32gui.GetWindow(self.hwndMain, win32con.GW_CHILD)
     
-    def screen_shot(self, emulator_name):
+    def screen_shot(self):
         # Get the dimensions of the window
         left, top, right, bot = win32gui.GetWindowRect(self.hwndChild)
         width = right - left
@@ -64,8 +104,8 @@ class LDPlayerInstance:
         saveDC.SelectObject(saveBitMap)
 
         # Copy the contents of the window to the bitmap
-        #result = saveDC.BitBlt((0, 0), (width, height), mfcDC, (0, 0), win32con.SRCCOPY)
-        #print(result)
+        result = saveDC.BitBlt((0, 0), (width, height), mfcDC, (0, 0), win32con.SRCCOPY)
+        print(result)
 
         # Save the bitmap as an image file
         bmpinfo = saveBitMap.GetInfo()
@@ -75,7 +115,7 @@ class LDPlayerInstance:
             (bmpinfo['bmWidth'], bmpinfo['bmHeight']),
             bmpstr, 'raw', 'BGRX', 0, 1)
 
-        img.save(f'screen_{emulator_name}.png')
+        img.save(f'screen_{self.window_name}.png')
 
         # Clean up resources
         win32gui.DeleteObject(saveBitMap.GetHandle())
@@ -91,10 +131,10 @@ class LDPlayerInstance:
         for key in keys:
             if isinstance(key, str):
                 vk_code = ord(key.upper())  # Get the virtual key code for the key
-                time1, time2 = load_config.PRESS_AND_HOLD.values()
+                time1, time2 = PRESS_AND_HOLD.values()
             else:
                 vk_code = key.value.vk  # Get the virtual key code from the Key enum
-                time1, time2 = load_config.TAP_AND_RELEASE.values()
+                time1, time2 = TAP_AND_RELEASE.values()
             win32gui.PostMessage(hwnd, win32con.WM_KEYDOWN, vk_code, 0)
             time.sleep(time1)  # Add a small delay after each key press
             win32gui.PostMessage(hwnd, win32con.WM_KEYUP, vk_code, 0)
@@ -136,16 +176,37 @@ class LDPlayerInstance:
     def activate_hypercharge(self):
         print(f'{self.window_name} - activating the hypercharge')
         self.send_keys_to_window(self.hwndChild, ['r'])
+    
+    def send_mouse_input(self, x, y, flags):
+        mouse_input = MouseInput(dx=x, dy=y, mouseData=0, dwFlags=flags, time=0, dwExtraInfo=None)
+        input_i = Input_I()
+        input_i.mi = mouse_input
+        input_obj = Input(type=ctypes.c_ulong(0), ii=input_i)
+        ctypes.windll.user32.SendInput(1, ctypes.byref(input_obj), ctypes.sizeof(input_obj))
+    
+    def manual_aim(self, enemy_distance, eppoint):
+        x, y = eppoint
+        actual_screen_width, actual_screen_height = 1920, 1080
+        img = Image.open(f'screen_{self.window_name}.png')
+        screenshot_screen_width, screenshot_screen_height = img.size
+        conversion_factor_x = actual_screen_width / screenshot_screen_width
+        conversion_factor_y = actual_screen_height / screenshot_screen_height
         
-    def manual_aim(x, y):
-        # This function is current in beta and might not work as intended...
-        win32api.SetCursorPos((x, y))
-        win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, x, y, 0, 0)
-        time.sleep(0.5)  # You may adjust the delay as needed
-        win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, x, y, 0, 0)
+        adjusted_x, adjusted_y = round(x * conversion_factor_x), round(y * conversion_factor_y)
         
+        # Check if the enemy is within shooting range
+        #if RANGE_LIMIT >= enemy_distance:
+            #print(f'Enemy distance is: {enemy_distance} and is within shooting range')
+
+        # Move the mouse cursor to the intersection point
+        self.send_keys_to_window(self.hwndChild, ['p'])
+        # Simulate aiming and shooting in the background
+        self.send_mouse_input(adjusted_x, adjusted_y, MOUSEEVENTF_RIGHTDOWN)
+        time.sleep(SHOT_DELAY)
+        self.send_mouse_input(adjusted_x, adjusted_y, MOUSEEVENTF_RIGHTUP)
+    
 # Example usage
-def start_action(action, instance):
-    thread = ActionThread(action, instance)
+def start_action(action, instance, enemy_distance, eppoint):
+    thread = ActionThread(action, instance, enemy_distance, eppoint)
     thread.start()
     return thread
