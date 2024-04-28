f76799b79f0ed5c6b26cf5bde4caae9e01729e63
Eforce <47955696+eforce67@users.noreply.github.com>
2024-04-27 20:51:53 -0800

    Update load_config.py
    updated configuration to suit manual aim logic

--- load_config.py
+++ load_config.py
@@ -3,22 +3,27 @@ import yaml
 with open('resources/config/settings.yaml', 'r') as configuration_file:
     config = yaml.safe_load(configuration_file)
 
-# bool constant
+# Boolean constants
 SOURCE_TRAINING = config.get('load_training', False)
 ENABLE_GPU = config.get('enable_gpu', False)
 
-# int/float constant
+# Integer/float constants
+SHOT_DELAY = config.get('shot_delay', 0.5)
+RANGE_LIMIT = config.get('range_distance_limit', 500)
 PRESS_AND_HOLD = config.get('press_&_hold')
 TAP_AND_RELEASE = config.get('tap_&_release')
 LOOPS = config.get('number_of_run', 50)
 CONFIDENCE = config.get('confidence_threshold', 0.8)
 PARALLEL_TRAINING = config.get('multi_parallel_training', ['LDPLayer'])
 if len(PARALLEL_TRAINING) > 3:
-    raise 'please check your parallel training setting in your config, it should have a max of no more than 3 emulator names...'
-WINNING = config.get('reward_rules')['reward_for_winning']
-LOSING = config.get('reward_rules')['reward_for_losing']
-DRAWING = config.get('reward_rules')['reward_for_draw']
-DYING = config.get('reward_rules')['reward_for_dying']
-SHOT_SUCCESS = config.get('reward_rules')['reward_for_successful_shooting']
-DAMAGE_TAKEN = config.get('reward_rules')['reward_for_taking_damage']
-WASTED_EQUIPMENT = config.get('reward_rules')['reward_for_wasting_equipment']
+    raise ValueError('Please check your parallel training setting in your config, it should have a maximum of no more than 3 emulator names...')
+
+# Reward rules
+REWARD_RULES = config.get('reward_rules')
+WINNING = REWARD_RULES.get('reward_for_winning')
+LOSING = REWARD_RULES.get('reward_for_losing')
+DRAWING = REWARD_RULES.get('reward_for_draw')
+DYING = REWARD_RULES.get('reward_for_dying')
+SHOT_SUCCESS = REWARD_RULES.get('reward_for_successful_shooting')
+DAMAGE_TAKEN = REWARD_RULES.get('reward_for_taking_damage')
+WASTED_EQUIPMENT = REWARD_RULES.get('reward_for_wasting_equipment')
